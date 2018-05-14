d50ef712392c1c31be7c8fc0b6fe001dcd385513
ks-ampersand <ks.ampersand@gmail.com>
2018-05-14 14:36:32 +0200

    modified sum.ts

--- week-02/day-01/sum.ts
+++ week-02/day-01/sum.ts
@@ -1,13 +1,12 @@
 // -  Write a function called `sum` that sum all the numbers until the given parameter
 // -  The function should return the result
 
-let x: number = 9;
 
-function sum1(): number {
+function sum1(x): number {
   let sum: number = 0;
     for (let i: number = 1; i < x; i++) {
         sum += i;
     }
     return sum;
 }
-console.log(sum1());
+console.log(sum1(9));
