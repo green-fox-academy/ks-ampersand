1b33263652b2072906a0d11aaee4f0cde38624a3
ks-ampersand <ks.ampersand@gmail.com>
2018-05-14 13:58:23 +0200

    compare-length.ts added

---
+++ week-02/day-01/compare-length.ts
@@ -0,0 +1,9 @@
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log to the console if `secondList` has more elements than `firstList`
+
+let firstList = [1, 2, 3];
+let secondList = [4, 5];
+console.log(secondList.length > firstList.length);
