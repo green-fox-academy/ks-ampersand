e881f65e5374575b7c1c2e4f11bd68abfa4a8174
ks-ampersand <ks.ampersand@gmail.com>
2018-05-14 15:22:40 +0200

    append-a.ts added

---
+++ week-02/day-01/append-a.ts
@@ -0,0 +1,11 @@
+// -  Create an array variable named `animals`
+//    with the following content: `["kuty", "macsk", "cic"]`
+// -  Add all elements an `"a"` at the end
+
+
+let animals = ["kuty", "macsk", "cic"];
+
+  animals = animals.map(function(elements) {
+      return `${elements}a`
+  });
+  console.log(animals);
