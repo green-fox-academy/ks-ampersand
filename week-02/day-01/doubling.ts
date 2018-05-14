59f8fab2d129294e5a4c1857a7e78d5f7a73c452
ks-ampersand <ks.ampersand@gmail.com>
2018-05-14 22:50:43 +0200

    Edited doubling.ts to remove obsolete code

--- week-02/day-01/doubling.ts
+++ week-02/day-01/doubling.ts
@@ -2,10 +2,9 @@
 // -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
 // -  Log the result of `doubling(baseNum)` to the console
 
-let basesum: number = 123;
 
-function doubling(): number {
+function doubling(basesum): number {
     return basesum * 2;
 }
 
-console.log(doubling());
+console.log(doubling(123));
