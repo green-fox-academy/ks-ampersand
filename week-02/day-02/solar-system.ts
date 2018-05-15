6fd59ce959cdf48ebe2909b48dc8aad325b4fdfb
ks-ampersand <ks.ampersand@gmail.com>
2018-05-15 15:44:54 +0200

    solar-system.ts modified: new solution added

--- week-02/day-02/solar-system.ts
+++ week-02/day-02/solar-system.ts
@@ -6,13 +6,25 @@
 let planetList: string[] = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Uranus', 'Neptune'];
 // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
 
-/*planetList.push('Saturn');
-console.log(planetList);*/
+planetList.push('Saturn');
+console.log(planetList);
 
-function putSaturn(planetList): string {
-    return `${planetList}, Saturn`;
+/* function putSaturn(array: string[]): string[] {
+    array = array.map(element => {element = `"${element}" `;
+    return element
+    });
+   array.push("Saturn");
+    return array;
 };
 
 console.log(putSaturn(planetList));
 
-export = putSaturn;
+export = putSaturn; */
+
+function putSaturn(array: string[]): void {
+   array.push(`Saturn"`);
+   console.log(array.join('", "'));
+   array.unshift('"');
+};
+
+console.log(putSaturn(planetList));
