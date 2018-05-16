db3302e281ac42aa72a49f44738a9891aac2b781
ks-ampersand <ks.ampersand@gmail.com>
2018-05-16 11:34:40 +0200

    Not so nice version of divide-by-zero.ts added

---
+++ week-02/day-03/divide-by-zero.ts
@@ -0,0 +1,18 @@
+'use strict'
+
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+let b: number;
+
+function divide(b): number {
+  if (b === 0) {
+    console.log('fail')
+  } else {
+  return 10 / b; 
+  }
+}
+
+console.log(divide(0));
