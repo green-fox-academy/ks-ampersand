ba00d11c756094f7a0b2558945876df7a1b8253c
ks-ampersand <ks.ampersand@gmail.com>
2018-05-23 17:08:57 +0200

    sharpie.ts changed to include optional parameter

--- week-03/day-03/sharpie.ts
+++ week-03/day-03/sharpie.ts
@@ -4,14 +4,16 @@ export class Sharpie {
   color: string;
   width: number;
   inkAmount: number;
-    constructor(color: string, width: number) {
-      this.inkAmount = 100;
+    constructor(color: string, width: number, inkAmount: number = 100) {
+      this.color = color;
+      this.width = width;
+      this.inkAmount = inkAmount;
     }
   use() {
     this.inkAmount -= 5;
   }
 }
 
-let pinkSharpie = new Sharpie('yellow', 3);
+/* let pinkSharpie = new Sharpie('yellow', 3);
 pinkSharpie.use();
-console.log(pinkSharpie.inkAmount);
+console.log(pinkSharpie); */
