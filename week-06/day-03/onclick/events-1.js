aec80e912f1edb6f0f25de38a3b9558747201f03
ks-ampersand <ks.ampersand@gmail.com>
2018-06-20 17:31:29 +0200

    Solution for the party button exercise added

---
+++ week-06/day-03/onclick/events-1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Turn the party on and off by clicking the button. (the whole page)
+
+let button = document.querySelector('button');
+let div = document.querySelector('div');
+
+div.classList.value = '';
+
+button.onclick = () => {
+  if (div.classList.value === '') {
+    div.classList.value = 'party';
+  } else {
+    div.classList.value = '';
+  }
+}; 
