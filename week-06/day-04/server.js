c9d6914c5dbfb4c6139ff3bde20a2920d264773b
ks-ampersand <ks.ampersand@gmail.com>
2018-06-21 11:22:48 +0200

    Finished with the servers

--- week-06/day-04/server.js
+++ week-06/day-04/server.js
@@ -2,10 +2,19 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = 3000;
 
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
 app.get('/', (req, res) => {
-  res.send('Such a smart demon!');
+  res.sendFile(path.join(__dirname, 'static/index.html'));
+});
+
+app.get('/greet/:message', (req, res) => {
+  console.log(req.query)
+  let name = 'Fox';
+  res.send(`${req.params.message}, ${req.query.name}`);
 });
 
 app.listen(PORT, () => {
