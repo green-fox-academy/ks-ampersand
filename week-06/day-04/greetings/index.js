0685d5b7e485d92e35f1db96680b5253df58feef
ks-ampersand <ks.ampersand@gmail.com>
2018-06-21 14:05:36 +0200

    Greetings exercose added

---
+++ week-06/day-04/greetings/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// home page
+app.get('/', (req, res) => {
+  console.log(req.query);
+  let name = 'Guest';
+  if (req.query.name !== undefined) {
+    name = req.query.name;
+  }
+  res.send(`Welcome back, ${name}!`);
+});
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
