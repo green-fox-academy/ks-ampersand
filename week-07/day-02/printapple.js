26177819ec498218d8ef0fc3bdae1d31e8bdd7c5
ks-ampersand <ks.ampersand@gmail.com>
2018-06-26 11:44:51 +0200

    Add solution for printapple exercise

---
+++ week-07/day-02/printapple.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Write a program that prints apple after 3 seconds.
+
+function printWhatever(string) {
+  console.log(string);
+}
+
+setTimeout(() => printWhatever('apple'), 3000);
