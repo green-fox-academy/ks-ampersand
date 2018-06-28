81e5282e461fee2112ff0402c638e9a1d2c04f0b
ks-ampersand <ks.ampersand@gmail.com>
2018-06-28 14:48:39 +0200

    get posts included

--- reddit/routes.js
+++ reddit/routes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+require('dotenv').config();
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
 app.use(express.json());
 app.use('/assets', express.static('assets'));
@@ -15,6 +24,20 @@ app.get('/', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send('What\'s up folks?');
 })
+
+//get posts
+app.get('/posts', (req, res) => {
+  conn.query('SELECT * FROM posts;', (err, posts) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      return;
+    }
+    res.json({
+      posts,
+    });
+  });
+});
 
 
 
